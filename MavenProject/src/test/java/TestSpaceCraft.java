526c17a2a099f0ace06b59f689dae906172b1821
smurph <56486787+mySmurph@users.noreply.github.com>
2021-12-02 11:42:41 -0800

    Added test

--- MavenProject/src/test/java/TestSpaceCraft.java
+++ MavenProject/src/test/java/TestSpaceCraft.java
@@ -5,18 +5,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestSpaceCraft {
+	
+	private SpaceCraft sc;
 
 	@Before
 	public void setUp() throws Exception {
+		sc = new SpaceCraft();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		sc = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBurnRate() {
+		int test_br = 15;
+		sc.setBurnRate(test_br);
+		
+		assertEquals(sc.getBurnRate(), test_br);
 	}
 
 }

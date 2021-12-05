3dc90a5e1464560f7a337b74db7d772edcab36fb
smurph <56486787+mySmurph@users.noreply.github.com>
2021-12-04 17:15:07 -0800

    breaking code to prove a piont

--- MavenProject/src/main/java/LunarLander.java
+++ MavenProject/src/main/java/LunarLander.java
@@ -2,7 +2,7 @@
 public class LunarLander {
 	
 	public static void main (String[] args){
-		GameController gc = new GameController();
+//		GameController gc = new GameController();	//break code
 		gc.run();
 	}
 	
